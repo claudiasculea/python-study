c72d53dc2bb15fa9c330fded5372a84f679cd9cf
Sculea Claudia <sculeaclaudia@sculeas-mbp.lan>
2022-08-18 15:20:39 +0200

    new file

---
+++ 3.1.Strings.py
@@ -0,0 +1,10 @@
+s = "Claudia"
+for index in range(len(s)):
+    if s[index] == "i" or s[index] == "u":
+        print("Your name has an i or an u")
+
+for char in s:
+    if char == "i" or char == "u":
+        print("Your name has an i or an u")
+
+        
