d946cf298357fe473269c7c366e66ffc83680055
Sculea Claudia <sculeaclaudia@sculeas-mbp.lan>
2022-08-18 14:38:45 +0200

    change 2

--- Cube root.py
+++ Cube root.py
@@ -1,4 +1,10 @@
 cube = int(input("Give me a number "))
-for guess in range(cube+1):
-    if guess**3 == cube:
-        print("cube root of", cube, "is", guess)
+for guess in range(abs(cube+1)):
+    if guess**3 >= abs(cube):
+        break
+
+if guess**3 != abs(cube):
+    print(cube, "is not a perfect cube")
+else:
+    print("cube root of", cube, "is", guess)
+
