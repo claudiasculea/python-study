23f22ebdd70111c3ba9c2ab8cc07930a0818e775
Sculea Claudia <sculeaclaudia@sculeas-mbp.lan>
2022-08-23 17:26:45 +0200

    sesion4

---
+++ A-Function.py
@@ -0,0 +1,7 @@
+def printName(firstName, lastName, reverse):
+    if reverse:
+        print(lastName, ",", firstName)
+    else:
+        print(firstName, lastName)
+
+printName("Claudia", "Sculea", False)
