7c7b89cbb7ef854c554f37144472e8fecb22de21
Sculea Claudia <sculeaclaudia@sculeas-mbp.lan>
2022-08-28 21:01:38 +0200

    session7

--- Fibonacci_recursion.py
+++ Fibonacci_recursion.py
@@ -7,4 +7,17 @@ def fib(x):
     else:
         return fib(x-1) + fib(x-2)
 
-print(fib(12))
+print(fib(6))
+
+#using dictionaries
+
+def fib_efficient(n, d): #n is the number and d is the dictionary
+    if n in d:
+        return d[n]
+    else:
+        ans = fib_efficient(n-1, d) + fib_efficient(n-2, d)
+        d[n] = ans
+        return ans
+
+d = {1:1, 2:2}
+print(fib_efficient(6, d))
