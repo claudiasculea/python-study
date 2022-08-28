0e134785f5bc1418c4f2a534c067829278e19ddc
Sculea Claudia <sculeaclaudia@sculeas-mbp.lan>
2022-08-28 20:55:04 +0200

    session7

--- Filter_with_dictionaries.py
+++ Filter_with_dictionaries.py
@@ -8,16 +8,32 @@ def lyrics_to_frequencies(lyrics):
             myDict[word] = 1
     return myDict
 
-print(lyrics_to_frequencies(our_lyrics))
+beatles = lyrics_to_frequencies(our_lyrics)
+print(beatles) #first we create a dictionary
 
 def most_common_words(freqs):
-    val = freqs.vars()
-    best = max(freqs.vars())
+    value = freqs.values() 
+    best = max(freqs.values())
     words = []
     for k in freqs:
         if freqs[k] == best:
             words.append(k)
     return(words, best)
 
-(w, b) = most_common_words(our_lyrics)
+(w, b) = most_common_words(beatles)
+print(w)
+print(b)
 
+def words_often(freqs, minTimes):
+    result = []
+    done = False
+    while not done:
+        temp = most_common_words(freqs)
+        if temp[1] >= minTimes:
+            result.append(temp)
+            for w in temp[0]:
+                del(freqs[w])
+        else:
+            done = True
+    return result
+print(words_often(beatles, 2))
