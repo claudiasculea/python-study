a0755a6b9bb054e72ec8c8f78170ed251d7b69a1
Sculea Claudia <sculeaclaudia@sculeas-mbp.lan>
2022-08-28 20:36:08 +0200

    folder6.3

--- Filter_with_dictionaries.py
+++ Filter_with_dictionaries.py
@@ -1,3 +1,4 @@
+our_lyrics = ['hey', 'ho', 'let/s', 'go', 'go']
 def lyrics_to_frequencies(lyrics):
     myDict = {}
     for word in lyrics:
@@ -7,4 +8,16 @@ def lyrics_to_frequencies(lyrics):
             myDict[word] = 1
     return myDict
 
-print(lyrics_to_frequencies(['la', 'la', 'la', 'la', 'la']))
+print(lyrics_to_frequencies(our_lyrics))
+
+def most_common_words(freqs):
+    val = freqs.vars()
+    best = max(freqs.vars())
+    words = []
+    for k in freqs:
+        if freqs[k] == best:
+            words.append(k)
+    return(words, best)
+
+(w, b) = most_common_words(our_lyrics)
+
