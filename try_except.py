e9630ed7b80dc49d2a3bac597c61cfa0ed3d1fda
Sculea Claudia <sculeaclaudia@sculeas-mbp.lan>
2022-08-30 14:21:24 +0200

    session8

---
+++ try_except.py
@@ -0,0 +1,8 @@
+try:
+    a = int(input('Give me a number: '))
+    b = int(input('Give me another number: '))
+    print(a/b)
+    print('okay')
+except:
+    print('bug in the system')
+print('outside of the loop')
